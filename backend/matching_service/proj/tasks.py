8db882e83d3033386a29b01e70722375b002e736
gaoyunfan <gaoyunfan8@gmail.com>
2024-09-20 23:15:46 +0800

    Implement initial user matching logic
    - Utilized two Redis sorted sets ordered by timestamp to track user matching requests.
    - Implemented logic to check for a complete match based on both topic and difficulty before attempting a partial match based only on the topic .
    - Ensured that users are matched within a 30-second window

--- backend/matching_service/proj/tasks.py
+++ backend/matching_service/proj/tasks.py
@@ -18,29 +18,39 @@ def process_matching_request(self, user_data):
     user_id = user_data["user_id"]
     topic = user_data["topic"]
     difficulty = user_data["difficulty"]
+
     print(
-        f"Processing request for user: {user_id}, topic: {topic}, difficulty: {difficulty}"
+        f"Received matching request: user_id={user_id}, topic={topic}, difficulty={difficulty}"
     )
 
-    # current_time = time.time()
-    # min_timestamp = current_time - 30
-    # key = f"matching:{topic}:{difficulty}"
-    # r.zremrangebyscore(key, "-inf", min_timestamp)
+    current_time = time.time()
+    min_timestamp = current_time - 30
+    key = f"matching:{topic}:{difficulty}"
+    r.zremrangebyscore(key, "-inf", min_timestamp)
 
-    # complete_matches = r.zrangebyscore(key, min_timestamp, "+inf")
-    # if len(complete_matches) > 0:
-    #     match = complete_matches[0]
-    #     r.zrem(key, match)
-    #     print(f"complete matched with: {match.decode('utf-8')}")
-    #     return
-    # r.zadd(key, {str(user_data): current_time})
+    complete_matches = r.zrangebyscore(key, min_timestamp, "+inf")
+    if len(complete_matches) > 0:
+        match = complete_matches[0]
+        r.zrem(key, match)
+        print(
+            f"Complete match found: user_id={user_id}, matched_with={match.decode('utf-8')}"
+        )
+        return
 
-    # topic_key = f"matching:{topic}"
-    # topic_matches = r.zrangebyscore(key, min_timestamp, "+inf")
-    # if len(topic_matches) > 0:
-    #     match = topic_matches[0]
-    #     r.zrem(key, match)
-    #     print(f"parital matched with: {match.decode('utf-8')}")
-    #     return
-
-    # r.zadd(topic_key, {str(user_data): current_time})
+    r.zadd(key, {str(user_data): current_time})
+    print(
+        f"Added user_id={user_id} to matching set for topic={topic} and difficulty={difficulty}"
+    )
+    topic_key = f"matching:{topic}"
+    topic_matches = r.zrangebyscore(topic_key, min_timestamp, "+inf")
+    if len(topic_matches) > 0:
+        match = topic_matches[0]
+        r.zrem(key, match)
+        print(
+            f"Partial match found: user_id={user_id}, matched_with={match.decode('utf-8')}"
+        )
+        return
+    r.zadd(topic_key, {str(user_data): current_time})
+    print(
+        f"No match found for user_id={user_id}, added to topic matching queue: {topic_key}"
+    )
