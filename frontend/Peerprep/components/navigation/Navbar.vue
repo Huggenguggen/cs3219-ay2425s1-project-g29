8bc3a5da5bd89529d59526e66f3aa5691fc0e014
Kidsnd274 <samuel_tee_gy@hotmail.com>
2024-10-05 04:23:03 +0800

    Added Admin button in Navbar
    Navbar can't properly check if User is an admin, might need state management

--- frontend/Peerprep/components/navigation/Navbar.vue
+++ frontend/Peerprep/components/navigation/Navbar.vue
@@ -3,12 +3,27 @@ import { signOut } from 'firebase/auth';
 
 const auth = useFirebaseAuth();
 const user = useCurrentUser();
+const isAdmin = ref(true);
 
 console.log("user", user)
 import NavLink from '~/components/navigation/NavLink.vue';
+
+const checkIfAdmin = async () => {
+    const tokenResult = await user.getIdTokenResult();
+    const adminResult = tokenResult.claims.admin === true;
+    isAdmin.value = adminResult;
+    console.log(adminResult);
+};
+
 function handleSignOut() {
     signOut(auth)
-}
+};
+
+watch(user, (newUser) => {
+    if (newUser) {
+        checkIfAdmin();
+    }
+});
 </script>
 
 
@@ -23,22 +38,27 @@ function handleSignOut() {
                 </div>
             </div>
 
-            <DropdownMenu>
-                <DropdownMenuTrigger as-child>
-                    <Avatar size="xs"
-                        class="hover:shadow-xl hover:bg-gray-300  transition-all duration-300 cursor-pointer">
-                        <AvatarImage :src="user.photoURL" alt="User Avatar" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
+            <div class="flex items-center space-x-4">
+                <template v-if="isAdmin">
+                    <NavLink to="/admin">Admin</NavLink>
+                </template>
+                <DropdownMenu>
+                    <DropdownMenuTrigger as-child>
+                        <Avatar size="xs"
+                            class="hover:shadow-xl hover:bg-gray-300  transition-all duration-300 cursor-pointer">
+                            <AvatarImage :src="user.photoURL" alt="User Avatar" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
 
-                </DropdownMenuTrigger>
-                <DropdownMenuContent class="w-fit" align="end">
-                    <DropdownMenuItem>
-                        <NuxtLink to="/users/settings">Settings</NuxtLink>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem @click="handleSignOut">Sign out</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent class="w-fit" align="end">
+                        <DropdownMenuItem>
+                            <NuxtLink to="/users/settings">Settings</NuxtLink>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem @click="handleSignOut">Sign out</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
     </nav>
 </template>
